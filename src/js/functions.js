b36d0ba396b7757269574fc4eda43c877296c903
Eliot Valdes <eliottvaldes@hotmail.com>
2022-11-05 19:50:02 -0600

    UPD -> remove unnecessary files/comments to use this template

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,19 +1,12 @@
 'use strict';
 
-// import the styles for this component
 import '../css/functions.css';
 
 const sayHello = (name) => {
-
-    console.log('Creating h1 element');
-
-    // create and h1 element and insert in the body
     const h1 = document.createElement('h1');
     h1.innerHTML = `Hello ${name}`;
 
-    // insert the h1 element in the body
     document.body.append(h1);
-
 }
 
 // export the module
--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,52 @@ const MiniCssExtract = require('mini-css-extract-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    // in this case, we want to use the webpack-dev-server
     mode: 'development',
 
-
-    // object to define the output of the webpack config
     output: {
-        clean: true, // clean the output folder before building
+        clean: true,
     },
 
-    // object with the modeule rules
     module: {
         rules: [
             {
-                test: /\.html$/, // regex to match the files to be processed
-                loader: 'html-loader', // loader to use
+                test: /\.html$/,
+                loader: 'html-loader',
                 options: {
-                    sources: false // disable the source map
+                    sources: false
                 }
             },
-            { // rules for the css files
-                test: /\.css$/, // regex to match the files to be processed
-                exclude: /styles.css$/, // exclude the global css file
-                use: ['style-loader', 'css-loader'] // array of loaders to use
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
             },
-            { // rules for the css files
-                test: /styles.css$/, // regex to match the files to be processed
-                use: [MiniCssExtract.loader, 'css-loader'] // array of loaders to use
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
             },
-            { // rules for the images
-                test: /\.(png|jpe?g|gif)$/, // regex to match the files to be processed
-                loader: 'file-loader' // loader to use
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
             }
         ]
     },
 
-    // optimizations obj
-    optimization: {},
-
-    // plugins arr
     plugins: [
         new HtmlWebpack({
-            filename: 'index.html', // path to the output file
-            title: 'Webpack using html-webpack-loader', // title of the page
-            template: './src/index.html', // path to the template
+            filename: 'index.html',
+            title: 'Webpack using html-webpack-loader',
+            template: './src/index.html',
         }),
-        // add the plugin to minify the css files and set a default name
         new MiniCssExtract({
             filename: '[name].css',
             ignoreOrder: false
         }),
-        // add the plugin to copy the assets folder to the dist folder
         new CopyPlugin({
             patterns: [
                 { from: 'src/assets/', to: 'assets/' }
             ]
         })
     ]
-
 
 }
